2e5074f5886fa7d1cf279cdb23aa1261feb257bb
MDaniyalTariq <153484599+MDaniyalTariq@users.noreply.github.com>
2024-06-04 10:00:07 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Real-time Object Detection with TensorFlow.js
+
+This repository contains a personal project demonstrating real-time object detection using TensorFlow.js. The project enables users to detect and classify multiple objects in live video streams, leveraging a pre-trained model.
+
+## Features:
+- **Webcam Integration**: Enable webcam access for real-time object detection directly within the browser.
+- **IP Webcam Support**: Option to use both local and IP webcams for flexible video input sources.
+- **Recording Capabilities**: Record and save video streams along with corresponding object detection data.
+- **Data Visualization**: Visualize object detection results through bounding boxes and labels directly on the live video stream.
+
+## Usage:
+1. Clone the repository to your local machine.
+2. Open `index.html` in a web browser.
+3. Follow the instructions to enable webcam access and start real-time object detection.
+
+## Getting Started:
+To get a local copy up and running, follow these simple steps:
+
+### Prerequisites:
+- Web browser with webcam support
+- Internet connection (for loading TensorFlow.js and pre-trained model)
+
+### Installation:
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/your-username/Realtime-Object-Detection-TensorFlowJS.git
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+const localWebcamButton = document.getElementById('localWebcamButton');
+const ipWebcamButton = document.getElementById('ipWebcamButton');
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
 const saveButton = document.getElementById('saveButton');
@@ -13,6 +14,9 @@ let combinedCanvas;
 let combinedCtx;
 let chunks = [];
 let mediaRecorder;
+let detectedObjects = {}; // Object to store detected objects and their counts
+let detectionsData = []; // Array to store detection data
+let usingLocalWebcam = true;
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
@@ -23,31 +27,78 @@ function getUserMediaSupported() {
 // wants to activate it to call enableCam function which we will 
 // define in the next step.
 if (getUserMediaSupported()) {
-    enableWebcamButton.addEventListener('click', enableCam);
+    localWebcamButton.addEventListener('click', enableLocalWebcam);
+    ipWebcamButton.addEventListener('click', enableIPWebcam);
 } else {
     console.warn('getUserMedia() is not supported by your browser');
 }
 
+// Add event listener to the upload button
+document.getElementById('uploadButton').addEventListener('click', enableUploadVideo);
+
 // Enable the live webcam view and start classification.
+function enableLocalWebcam(event) {
+    usingLocalWebcam = true;
+    enableCam(event);
+}
+
+function enableIPWebcam(event) {
+    usingLocalWebcam = false;
+    enableCam(event);
+}
+
+function enableUploadVideo(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    enableCam(event);
+}
+
 function enableCam(event) {
     // Only continue if the COCO-SSD has finished loading.
     if (!model) {
         return;
     }
-    
-    // Hide the button once clicked.
-    event.target.classList.add('removed');  
-    
-    // getUsermedia parameters to force video but not audio.
-    const constraints = {
-        video: true
-    };
 
-    // Activate the webcam stream.
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        video.srcObject = stream;
-        video.addEventListener('loadeddata', predictWebcam);
-    });
+    // Hide the button once clicked.
+    event.target.classList.add('removed');
+
+    // Check if a video file has been uploaded
+    const videoUpload = document.getElementById('videoUpload');
+    if (videoUpload.files.length > 0) {
+        const uploadedVideo = videoUpload.files[0];
+        video.src = URL.createObjectURL(uploadedVideo);
+        video.addEventListener('loadeddata', () => {
+            predictVideo();
+        });
+    } else {
+        // Determine the video source URL based on whether it's a local webcam or IP webcam
+        let videoSource;
+        if (usingLocalWebcam) {
+            videoSource = 'local';
+        } else {
+            // Replace 'YOUR_IP_WEBCAM_URL' with the actual URL of your IP webcam stream
+            videoSource = 'http://192.168.1.3:8080/video';
+        }
+
+        // Activate the webcam stream.
+        if (videoSource === 'local') {
+            const constraints = {
+                video: true
+            };
+            navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+                video.srcObject = stream;
+                video.addEventListener('loadeddata', () => {
+                    predictVideo();
+                });
+            });
+        } else {
+            document.getElementById('liveView').value = videoSource;
+            // Set the src attribute of the video element to the IP webcam URL
+            video.src = videoSource;
+            video.addEventListener('loadeddata', () => {
+                predictVideo();
+            });
+        }
+    }
 }
 
 // Pretend model has loaded so we can try out the webcam code.
@@ -74,7 +125,7 @@ cocoSsd.load().then(function (loadedModel) {
     liveView.appendChild(combinedCanvas);
 });
 
-function predictWebcam() {
+function predictVideo() {
     // Now let's start classifying a frame in the stream.
     model.detect(video).then(function (predictions) {
         // Draw video frame
@@ -97,11 +148,25 @@ function predictWebcam() {
                     bbox[0],
                     bbox[1] > 10 ? bbox[1] - 5 : 10
                 );
+
+                // Increment the count of detected object
+                if (detectedObjects.hasOwnProperty(predictions[n].class)) {
+                    detectedObjects[predictions[n].class]++;
+                } else {
+                    detectedObjects[predictions[n].class] = 1;
+                }
+
+                // Store detection data
+                detectionsData.push({
+                    class: predictions[n].class,
+                    score: predictions[n].score,
+                    bbox: bbox
+                });
             }
         }
 
         // Call this function again to keep predicting when the browser is ready.
-        window.requestAnimationFrame(predictWebcam);
+        window.requestAnimationFrame(predictVideo);
     });
 }
 
@@ -112,6 +177,7 @@ saveButton.addEventListener('click', saveRecording);
 
 function startRecording() {
     chunks = [];
+    detectionsData = []; // Reset detection data
     const stream = combinedCanvas.captureStream();
     mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.ondataavailable = function(event) {
@@ -124,16 +190,51 @@ function startRecording() {
 
 function stopRecording() {
     mediaRecorder.stop();
+    // saveRecording();
 }
 
 function saveRecording() {
-    const blob = new Blob(chunks, { type: 'video/webm' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style = 'display: none';
-    a.href = url;
-    a.download = 'recorded_video.webm';
-    a.click();
-    window.URL.revokeObjectURL(url);
+    // Save the detected objects, their counts, and detection data to a text file
+    let textToWrite = '';
+
+    // Detected objects and their counts
+    for (const [object, count] of Object.entries(detectedObjects)) {
+        textToWrite += `${object} ${count}\n`;
+    }
+
+    // Append the total count of each object at the end
+    textToWrite += '\nTotal Counts:\n';
+    for (const [object, count] of Object.entries(detectedObjects)) {
+        textToWrite += `${object} ${count}\n`;
+    }
+
+    // Detection data
+    for (let i = 0; i < detectionsData.length; i++) {
+        const obj = detectionsData[i];
+        textToWrite += `${obj.class} - ${Math.round(obj.score * 100)}%: ${obj.bbox}\n`;
+    }
+
+    const textBlob = new Blob([textToWrite], { type: 'text/plain' });
+
+    // Save video and text files together
+    const allChunks = chunks.concat(textBlob); // Combine video chunks with text file
+
+    const videoBlob = new Blob(allChunks, { type: 'video/webm' });
+    const videoUrl = URL.createObjectURL(videoBlob);
+    const aVideo = document.createElement('a');
+    document.body.appendChild(aVideo);
+    aVideo.style = 'display: none';
+    aVideo.href = videoUrl;
+    aVideo.download = 'recorded_video.webm';
+    aVideo.click();
+    window.URL.revokeObjectURL(videoUrl);
+
+    const textUrl = URL.createObjectURL(textBlob);
+    const aText = document.createElement('a');
+    document.body.appendChild(aText);
+    aText.style = 'display: none';
+    aText.href = textUrl;
+    aText.download = 'recorded_data.txt';
+    aText.click();
+    window.URL.revokeObjectURL(textUrl);
 }
